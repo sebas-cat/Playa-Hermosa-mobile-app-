0816f0e6866a8ec55e2633cd181f615933dd7c91
Sebas <sebastia.vargas.carva@gmail.com>
2025-11-12 21:29:34 -0600

    Add project files.

---
+++ Infohotel/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Infohotel
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Infohotel/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿
+
+namespace Infohotel
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnPrevClicked(object sender, EventArgs e)
+        {
+            try
+            {
+               
+                if (carousel.Position > 0)
+                    carousel.Position--;
+                else
+                    carousel.Position = GetItemsCount() - 1;
+            }
+            catch { }
+        }
+
+        void OnNextClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                int count = GetItemsCount();
+                if (carousel.Position < count - 1)
+                    carousel.Position++;
+                else
+                    carousel.Position = 0;
+            }
+            catch { }
+        }
+
+        int GetItemsCount()
+        {
+  
+            if (carousel.ItemsSource is System.Collections.IEnumerable en)
+            {
+                int c = 0;
+                foreach (var _ in en) c++;
+                return c;
+            }
+            return 0;
+        }
+    }
+
+}
