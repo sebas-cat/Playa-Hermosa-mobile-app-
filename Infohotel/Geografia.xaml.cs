2715225f7f97b99e4a638f23ccbe9550f78a15c3
ANCOTOM <angulocorderotomas27@gmail.com>
2025-12-16 01:52:52 -0600

    Mapa Completamente Funcional

--- Infohotel/Geografia.xaml.cs
+++ Infohotel/Geografia.xaml.cs
@@ -1,3 +1,6 @@
+using Microsoft.Maui.Controls.Maps;
+using Microsoft.Maui.Maps;
+
 namespace Infohotel;
 
 public partial class Geografia : ContentPage
@@ -5,5 +8,27 @@ public partial class Geografia : ContentPage
 	public Geografia()
 	{
 		InitializeComponent();
+        CargarMapa();
 	}
+
+    private void CargarMapa()
+    {
+        var ubicacion = new Location(10.572118733917682, -85.6972728693201); 
+
+        HotelMapa.MoveToRegion(
+            MapSpan.FromCenterAndRadius(
+                ubicacion,
+                Distance.FromKilometers(1))
+        );
+
+        var pin = new Pin
+        {
+            Label = "Waldorf Astoria",
+            Address = "Guanacaste",
+            Location = ubicacion,
+            Type = PinType.Place
+        };
+
+        HotelMapa.Pins.Add(pin);
+    }
 }
