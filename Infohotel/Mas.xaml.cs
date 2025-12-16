05db13911ad7f34430acce324c3ebe9a26bd3ea8
SebasMViquez <viquezdayanna81@gmail.com>
2025-12-16 10:26:09 -0600

    Pagina de ayuda(Mas.xaml)

--- Infohotel/Mas.xaml.cs
+++ Infohotel/Mas.xaml.cs
@@ -2,8 +2,12 @@ namespace Infohotel;
 
 public partial class Mas : ContentPage
 {
-	public Mas()
-	{
-		InitializeComponent();
-	}
+    public Mas()
+    {
+        InitializeComponent();
+        HotelWebView.Source = "https://www.hilton.com/es/hotels/lirguwa-waldorf-astoria-costa-rica-punta-cacique/";
+    }
+
+   
 }
+
